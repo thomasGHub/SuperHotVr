1c81b76735f0c433fca1bddc5d7c28aea2b10f84
DabertLancelot <97019764+DabertLancelot@users.noreply.github.com>
2022-02-22 14:21:17 +0100

    maj

--- Assets/tirer.cs
+++ Assets/tirer.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class tirer : MonoBehaviour
 {   [SerializeField] private GameObject cible;
     [SerializeField] private GameObject arme;
+    private float dist;
     // Start is called before the first frame update
     void Start()
     {
-        
+       dist = Vector3.Distance(cible.transform.position, transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        dist = Vector3.Distance(cible.transform.position, transform.position);
+        if (dist < 20)
+        {
+
+        }
     }
 }
